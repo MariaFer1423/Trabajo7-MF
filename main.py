5dd6f27a4b6b876c4b51e09a210e329d6aae8fbf
MariaFer1423 <fernanda.camacho@ulead.ac.cr>
2024-04-09 13:47:53 -0600

    Prediction added to the json response

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ async def predict(
     try:
         df = pd.DataFrame(dictionary, index=[0])
         prediction = model.predict(df)
+        prediction = int(prediction[0])
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"prediction":prediction}
